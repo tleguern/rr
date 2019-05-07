3135a56fe948cc28ed466ac2f16a01ed9e63ade4
Tristan Le Guern <tleguern@bouledef.eu>
2019-05-07 19:08:15 +0100

    Reindent

--- rr.sh
+++ rr.sh
@@ -19,12 +19,12 @@ set -e
 
 unset -f get_random
 if command -v jot > /dev/null 2>&1; then
-  get_random() { jot -r 1 1 $1 2> /dev/null; }
+	get_random() { jot -r 1 1 $1 2> /dev/null; }
 elif command -v shuf > /dev/null 2>&1; then
-  get_random() { shuf -i 1-$1 -n 1 2> /dev/null; }
+	get_random() { shuf -i 1-$1 -n 1 2> /dev/null; }
 else
-  # Neither jot(1) nor shuf(1). What are you doing with your life?
-  get_random() { echo $((RANDOM % ($1 + 1))); }
+	# Neither jot(1) nor shuf(1). What are you doing with your life?
+	get_random() { echo $((RANDOM % ($1 + 1))); }
 fi
 
 commands=$(mktemp)
@@ -36,10 +36,10 @@ trap '{ rm -f -- $commands $commands_subset; }' EXIT
 OLDIFS="$IFS"
 IFS=':'
 for path in $PATH; do
-  # Cope with bogus $PATH
-  if [ ! -d "$path" ]; then
-    continue
-  fi
+	# Cope with bogus $PATH
+	if [ ! -d "$path" ]; then
+		continue
+	fi
 	find "$path" -mindepth 1 -maxdepth 1 -print >> $commands
 done
 IFS="$OLDIFS"
@@ -47,7 +47,7 @@ IFS="$OLDIFS"
 # Randomly select five commands to mix with rm
 nline="$(cat $commands | wc -l | tr -d ' ')"
 for i in 0 1 2 3 4; do
-  head -n $(get_random $nline) $commands | tail -n 1 >> $commands_subset
+	head -n $(get_random $nline) $commands | tail -n 1 >> $commands_subset
 done
 echo /bin/rm >> $commands_subset
 
